3e628f990e68e75d42728c3546f1aed97c6c876a
mkjubran <mkjubran@github.com>
2018-03-25 20:57:27 +0100

    Ver0.2

--- HMS/source/Lib/TLibCommon/CommonDef.h
+++ HMS/source/Lib/TLibCommon/CommonDef.h
@@ -120,9 +120,9 @@ static const Double MAX_DOUBLE =                             1.7e+308; ///< max.
 // ====================================================================================================================
 // Most of these should not be changed - they resolve the meaning of otherwise magic numbers.
 
-static const Int MAX_GOP =                                         64; ///< max. value of hierarchical GOP size
-static const Int MAX_NUM_REF_PICS =                                32; ///< max. number of pictures used for reference // Jubran
-static const Int MAX_NUM_REF =                                     32; ///< max. number of entries in picture reference list // Jubran
+static const Int MAX_GOP =                                       256; ///< max. value of hierarchical GOP size
+static const Int MAX_NUM_REF_PICS =                              1024; ///< max. number of pictures used for reference // Jubran
+static const Int MAX_NUM_REF =                                   1024; ///< max. number of entries in picture reference list // Jubran
 static const Int MAX_QP =                                          51;
 static const Int NOT_VALID =                                       -1;
 
