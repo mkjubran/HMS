90c817f765a923b9c25737c2949207e4f294077a
mkjubran <mkjubran@github.com>
2018-03-29 13:01:42 +0100

    Ver2.8

--- CreateHMGOBStruct.py
+++ CreateHMGOBStruct.py
@@ -7,50 +7,61 @@ with open('OrderedFrames.txt') as f:
     FNums = f.readlines()
 f.close()
 
-cnt=0
-ref_pics_active=0
-ref_pics=0
-ref_pics_active_Max=4
-ref_pics_Max=6
-ref_pics_active_Stitching=1
-ref_pics_array=[]
-iFNum=[]
+ref_pics_active_Max=5
+ref_pics_active_Stitching=3
+
+iFNums=map(int, FNums)
+GOB=len(iFNums)
 
 fid = open('encoder_HMS_GOB.cfg','w')
+print >> fid, '#======== Coding Structure ============='
+print >> fid, 'IntraPeriod                   : -1           # Period of I-Frame ( -1 = only first)'
+print >> fid, 'DecodingRefreshType           : 2           # Random Accesss 0:none, 1:CRA, 2:IDR, 3:Recovery Point SEI'
+print >> fid, 'GOPSize                       : '+str(GOB-1)+'           # GOP Size (number of B slice = GOPSize-1)'
+print >> fid, 'ReWriteParamSetsFlag          : 1           # Write parameter sets with every IRAP'
+'#        Type POC QPoffset QPOffsetModelOff QPOffsetModelScale CbQPoffset CrQPoffset QPfactor tcOffsetDiv2 betaOffsetDiv2 temporal_id #ref_pics_active #ref_pics reference pictures     predict deltaRPS' '#ref_idcs reference idcs'
+print >> fid,''
 
-for FNum in FNums:
-	cnt=cnt+1
-	i=0
-	j=0
-	FNum=FNum.rstrip()
-	iFNum.append(int(FNum))
-	ref_pics_array[:]=[]
-	if ref_pics_active < ref_pics_active_Max:
-		ref_pics_active=ref_pics_active+1
-	else:
-		ref_pics_active=ref_pics_active_Max
 
-        if ref_pics < ref_pics_Max:
-		ref_pics=ref_pics+1
-	else:
-		ref_pics=ref_pics_Max
+iFNums_array = np.array(iFNums)
+ref_pics_Stitching_array=iFNums_array[0:ref_pics_active_Stitching]
+ref_pics_RemovedStitching_array=iFNums_array[ref_pics_active_Stitching:GOB]
+ref_pics_RemovedStitching_array.sort()
 
-	for i in range(min(cnt,ref_pics_active_Stitching)):
- 		ref_pics_array.append(iFNum[i])
-		print('--->',cnt,i)
+iFNums_array=np.concatenate((ref_pics_Stitching_array,ref_pics_RemovedStitching_array), axis=0) #Stitching Frames + Ordered remaining Frames
 
-	for j in range((min(cnt,ref_pics_active_Stitching)+1),min(cnt,ref_pics_active)):
- 		ref_pics_array.append(j)
-		print('+++++>',cnt,i)
-	
-	print(iFNum)
-	print(ref_pics_array[:])
-
-	GOBLine='Frame' + str(cnt) + ': P '+ str(FNum) +' 0 -6.5 0.2590 0 0 1.0 0 0 0 '+ str(ref_pics_active) + ' ' + str(ref_pics) + ' -1 0'
-
+for cnt in range(1,ref_pics_active_Stitching+1):
+	GOBLine=''
+	GOBLine='Frame' + str(cnt) + ': P '+ str(iFNums_array[cnt]) +' 0 -6.5 0.2590 0 0 1.0 0 0 0 '+ str(cnt) + ' ' + str(cnt)
+	for cnt1 in range(cnt):
+		GOBLine=GOBLine+' '+str(iFNums_array[cnt1]-iFNums_array[cnt])
+	GOBLine=GOBLine+' 0'
 	print >> fid, GOBLine
 	
-	
+
+for cnt in range(ref_pics_active_Stitching+1,GOB):
+	GOBLine=''
+	cnt3=-1
+	if cnt < ref_pics_active_Max+1:
+		GOBLine='Frame' + str(cnt) + ': P '+ str(iFNums_array[cnt]) +' 0 -6.5 0.2590 0 0 1.0 0 0 0 '+ str(cnt) + ' ' + str(cnt)
+		for cnt1 in range(cnt):
+			if cnt1<ref_pics_active_Stitching:
+				GOBLine=GOBLine+' '+str(ref_pics_Stitching_array[cnt1]-iFNums_array[cnt])
+			else:
+				GOBLine=GOBLine+' '+str(cnt3)
+				cnt3=cnt3-1
+
+	else:
+		GOBLine='Frame' + str(cnt) + ': P '+ str(iFNums_array[cnt]) +' 0 -6.5 0.2590 0 0 1.0 0 0 0 '+ str(ref_pics_active_Max) + ' ' + str(ref_pics_active_Max)
+		for cnt1 in range(ref_pics_active_Max):
+			if cnt1<ref_pics_active_Stitching:
+				GOBLine=GOBLine+' '+str(ref_pics_Stitching_array[cnt1]-iFNums_array[cnt])
+			else:
+				GOBLine=GOBLine+' '+str(cnt3)
+				cnt3=cnt3-1
+	GOBLine=GOBLine+' 0'
+	print >> fid, GOBLine
+
 f.close()    	
 
 
