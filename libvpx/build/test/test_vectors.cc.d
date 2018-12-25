a95c56a105fc44a02c9750e2c9300463cb6cdaa8
Mohammad Jubran <mjubran@birzeit.edu>
2018-12-26 01:19:39 +0200

    Ver15.4

--- libvpx/build/test/test_vectors.cc.d
+++
@@ -1,2 +0,0 @@
-test/test_vectors.cc.o test/test_vectors.cc.d: ../test/test_vectors.cc /usr/include/stdc-predef.h \
- ../test/test_vectors.h vpx_config.h
--- libvpx/build/vpx_dsp/skin_detection.c.d
+++
@@ -1,2 +0,0 @@
-vpx_dsp/skin_detection.c.o vpx_dsp/skin_detection.c.d: ../vpx_dsp/skin_detection.c /usr/include/stdc-predef.h \
- ../vpx_dsp/skin_detection.h
