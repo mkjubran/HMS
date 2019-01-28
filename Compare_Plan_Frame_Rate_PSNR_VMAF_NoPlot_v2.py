58149610db2d5abf6ec88b256bdff0952ce368d4
Mohammad Jubran <mjubran@birzeit.edu>
2019-01-28 16:10:48 +0200

    Ver16.4

---
+++ Compare_Plan_Frame_Rate_PSNR_VMAF_NoPlot_v2.py
@@ -0,0 +1,176 @@
+#python ./Compare_Rate_PSNR_Frame.py --fn1=../HMSsync/HMSResu --fn2=../HMSsync/HMSRe
+
+from __future__ import division
+import numpy as np
+import os, sys, subprocess, pdb
+import argparse, re
+import matplotlib.pyplot as plt
+
+
+# Instantiate the parser
+parser = argparse.ArgumentParser(description='Optional app description')
+
+# Optional argument
+parser.add_argument('--fn1', type=str,
+                    help='file name of first Rate_PSNR file')
+
+parser.add_argument('--Lfn1', type=str,
+                    help='Label of legend in plots related to first Rate_PSNR file')
+
+parser.add_argument('--fn2', type=str,
+                    help='file name of second Rate_PSNR file')
+
+parser.add_argument('--Lfn2', type=str,
+                    help='Label of legend in plots related to second Rate_PSNR file')
+
+parser.add_argument('--fps', type=int,
+                    help='frame per second')
+
+parser.add_argument('--nf', type=int,
+                    help='number of frames')
+
+
+args = parser.parse_args()
+
+def Get_FNum_Rate_PSNR(fname):
+   #read file
+   with open(fname) as f:
+      fl = f.readlines()
+      f.close()
+
+   ## get rate and PSNR
+   FNum_Rate_PSNR=np.empty((1,3), float)
+   for cnt in range(len(fl)):
+      L=fl[cnt].replace("..."," ")
+      L=L.replace("'","")
+      L=L.replace(",","").split()
+      if len(L)>0:
+         if L[0]=='POC':
+             #print(L)
+             FNum=int(L[1])+0
+             FRate=int(L[11])+0
+             FPSNR=float(L[14])+0
+	     FNum_Rate_PSNR=np.append(FNum_Rate_PSNR,np.array([[FNum,FRate,FPSNR]]),0)
+   FNum_Rate_PSNR=FNum_Rate_PSNR[1:np.size(FNum_Rate_PSNR,0)]
+   return FNum_Rate_PSNR
+
+def Comp_TotalRate(f_FNum_Rate_PSNR):
+   TotalSize=0
+   for cnt in range(np.shape(f_FNum_Rate_PSNR)[0]):
+	TotalSize=TotalSize+f_FNum_Rate_PSNR[cnt,1]
+   TotalRate=((TotalSize/np.shape(f_FNum_Rate_PSNR)[0])*fps)/1000
+   return TotalRate
+   
+
+def Comp_TotalPSNR(f_FNum_Rate_PSNR):
+   TotalMSE=0
+   for cnt in range(np.shape(f_FNum_Rate_PSNR)[0]):
+	TotalMSE=TotalMSE+( (255**2) / (10**(f_FNum_Rate_PSNR[cnt,2]/10)) )
+   
+   TotalMSE=TotalMSE/int(np.shape(f_FNum_Rate_PSNR)[0])
+   TotalPSNR=10*np.log10(((255**2)/(TotalMSE)))
+   return TotalPSNR
+
+
+def Get_FNum_VMAF(fname):
+   #read file
+   with open(fname) as f:
+      fl = f.readlines()
+      f.close()
+
+   ## get VMAF Score
+   FNum_VMAF=np.empty((1,2), float)
+   for cnt in range(len(fl)):
+      L=fl[cnt].replace("..."," ")
+      L=L.replace("'","")
+      L=L.replace("\\n","")
+      L=L.replace("]","")
+      L=L.replace(":"," ")
+      L=L.replace(",","").split()
+      if len(L)>0:
+         if L[0]=='VMAF_Frame':
+             #print(L)
+             #print('{}...{}'.format(L[1],L[15]))
+             FNum=int(L[1])+0
+             FVMAF=float(L[15])+0
+	     FNum_VMAF=np.append(FNum_VMAF,np.array([[FNum,FVMAF]]),0)
+   FNum_VMAF=FNum_VMAF[1:np.size(FNum_VMAF,0)]
+   return FNum_VMAF
+
+def Comp_TotalVMAF(f_FNum_VMAF):
+   TotalVMAF=0
+   for cnt in range(np.shape(f_FNum_VMAF)[0]):
+	TotalVMAF=TotalVMAF+f_FNum_VMAF[cnt,1]
+   TotalVMAF=(TotalVMAF/np.shape(f_FNum_VMAF)[0])
+   return TotalVMAF
+
+if __name__ == '__main__':
+   ##Inputs
+   nf=0;
+   fname1=args.fn1;
+   fname2=args.fn2;
+   Lfname1=args.Lfn1;
+   Lfname2=args.Lfn2;
+   fps=args.fps;
+   nf=args.nf;
+   #np.set_printoptions(threshold=np.nan)
+   
+   f1_FNum_Rate_PSNR=Get_FNum_Rate_PSNR(fname1)
+   f2_FNum_Rate_PSNR=Get_FNum_Rate_PSNR(fname2)
+
+   f1_FNum_VMAF=Get_FNum_VMAF(fname1)
+   f2_FNum_VMAF=Get_FNum_VMAF(fname2)
+
+   f1_numFrames=np.shape(f1_FNum_Rate_PSNR)[0]
+   f2_numFrames=np.shape(f2_FNum_Rate_PSNR)[0]
+
+   ## to consider same number of frames of both files
+   if f1_numFrames > f2_numFrames:
+      f1_FNum_Rate_PSNR=f1_FNum_Rate_PSNR[0:f2_numFrames,:]
+      f1_FNum_VMAF=f1_FNum_VMAF[0:f2_numFrames,:]
+      nfc=f2_numFrames
+   else:
+      f2_FNum_Rate_PSNR=f2_FNum_Rate_PSNR[0:f1_numFrames,:]
+      f2_FNum_VMAF=f2_FNum_VMAF[0:f1_numFrames,:]
+      nfc=f1_numFrames
+
+   if (nf != 0) and (nf <= f1_numFrames) and (nf <= f2_numFrames):
+	f1_FNum_Rate_PSNR=f1_FNum_Rate_PSNR[0:nf,:]
+	f2_FNum_Rate_PSNR=f2_FNum_Rate_PSNR[0:nf,:]
+	f1_FNum_VMAF=f1_FNum_VMAF[0:nf,:]
+	f2_FNum_VMAF=f2_FNum_VMAF[0:nf,:]
+	nfc=nf
+
+## compute the over all: Number of Frames, Rate, PSNR
+   TotalRatefn1=Comp_TotalRate(f1_FNum_Rate_PSNR)
+   TotalRatefn2=Comp_TotalRate(f2_FNum_Rate_PSNR)
+
+   TotalPSNRfn1=Comp_TotalPSNR(f1_FNum_Rate_PSNR)
+   TotalPSNRfn2=Comp_TotalPSNR(f2_FNum_Rate_PSNR)
+
+   TotalVMAFfn1=Comp_TotalVMAF(f1_FNum_VMAF)
+   TotalVMAFfn2=Comp_TotalVMAF(f2_FNum_VMAF)
+
+#   print("Number of frames used to compute Rate, PSNR and VMAR is {}").format(nfc)
+#   print("Fn1 ({}): #Frames= {}, Rate={} kbps, PSNR={} dB, VMAF={}").format(Lfname1,f1_numFrames,TotalRatefn1,TotalPSNRfn1,TotalVMAFfn1)
+#   print("Fn2 ({}): #Frames= {}, Rate={} kbps, PSNR={} dB, VMAF={}").format(Lfname2,f2_numFrames,TotalRatefn2,TotalPSNRfn2,TotalVMAFfn2)
+
+   T1=fname1.split('.da');
+   T2=T1[0].split('_');
+   for i in T2:
+	if ((i[0:4]=='rate') or (i[0:4]=='Rate')):
+	    R1=i[4:len(i)]
+	    R1=int(R1)/1000
+
+   T1=fname2.split('.da');
+   T2=T1[0].split('_');
+   for i in T2:
+	if ((i[0:4]=='rate') or (i[0:4]=='Rate')):
+	    R2=i[4:len(i)]
+	    R2=int(R2)/1000
+
+   print("{}	{}	{}	{}	{}").format(f1_numFrames,int(R1),TotalRatefn1,TotalPSNRfn1,TotalVMAFfn1)
+   print("{}	{}	{}	{}	{}").format(f2_numFrames,int(R2),TotalRatefn2,TotalPSNRfn2,TotalVMAFfn2)
+
+   plt.show()
+
