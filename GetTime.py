b6c59cb2b9996cbb1eb16aad159fc7f5d6685071
Mohammad Jubran <mjubran@birzeit.edu>
2019-06-23 15:55:13 +0300

    Ver17.4

---
+++ GetTime.py
@@ -0,0 +1,9 @@
+#Frame1: Type POC QPoffset QPOffsetModelOff QPOffsetModelScale CbQPoffset CrQPoffset QPfactor tcOffsetDiv2 betaOffsetDiv2 temporal_id #ref_pics_active #ref_pics reference 
+#pictures predict deltaRPS #ref_idcs reference idcs print >> fid, 'Frame1: P 1 5 -6.5 0.2590 0 0 1.0 0 0 0 1 1 -1 0');
+import datetime, math, time
+import sys
+
+curr_time=datetime.datetime.now();
+fid = open('../savenpy/MemoryTime_log.txt','a+')
+fid.write('Current {} Time = {}\n'.format(sys.argv[1],curr_time))
+
